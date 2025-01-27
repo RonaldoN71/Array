8af7b2a346c4024690dae0f6b29e5933fc9c1910
Ronaldo Nandeibam <ronaldonandeibam7@gmail.com>
2025-01-27 20:27:13 +0530

    Add files via upload

---
+++ DSA12.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class DSA12 {
+    public static void main(String[]args){
+        int a[] = {3, 5, 4, 1, 9};
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<a.length;i++){
+            if(min>a[i]){
+                min = a[i];
+            }
+            if(a[i]>max){
+                max = a[i];
+            }
+        }
+        System.out.println("Min and Max are: "+min+" "+max);
+
+    }
+}
