c4ed985a030aa1d47db7c4b5aac0abe94709c7b3
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-01-28 12:42:48 +0530

    new file

--- DSA14.java
+++ DSA14.java
@@ -4,19 +4,20 @@ public class DSA14 {
         int max = Integer.MIN_VALUE;
         for(int i=0;i<n;i++ ){
             for(int j=i;j<n;j++){
-                int count=0;
-                for(int k=i;k<j;k++){
-                    count+=a[k];
-                    if(count>max){
-                        max = count;
-                    }
+                int count =0;
+                for(int k=i;k<=j;k++){
+                  count +=a[k];
+                  if(count>max){
+                    max = count;
+                  }
                 }
+               
             }
         }
         System.out.println(max);
     }
     public static void main(String[]args){
-         int a[] = {-2,1,-3,4,-1,2,1,-5,4};
+         int a[] = {1,2,3,4};
     MaxSub(a);
     }
    
