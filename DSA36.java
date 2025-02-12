88891a32122e32ff7fe694eb663a1740328ef81b
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-02-12 11:43:28 +0530

    new file

---
+++ DSA36.java
@@ -0,0 +1,12 @@
+public class DSA36 {
+    public static void main(String[] args) {
+        int []a = {1,2,3,4,5};
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                System.out.print("["+a[i]+" "+a[j]+"] ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
